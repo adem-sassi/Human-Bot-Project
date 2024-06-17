fb10537a02a87e44e1a27bf4325b9fe250e68f8b
azizkab <azizkabissa007@gmail.com>
2024-06-17 15:58:56 +0200

    updated version of the schema

--- hb_schema.sql
+++ hb_schema.sql
@@ -1,39 +1,43 @@
--- Table EMPLOYEES
-CREATE TABLE employees (
-    employee_id SERIAL PRIMARY KEY,
-    firstname VARCHAR(50) NOT NULL,
-    lastname VARCHAR(50) NOT NULL,
-    age INTEGER NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE
-);
-
--- Table SUPPLIERS
-CREATE TABLE suppliers (
-    supplier_id SERIAL PRIMARY KEY,
-    supplier_name VARCHAR(100) NOT NULL
-);
-
--- Table DELIVERIES
-CREATE TABLE deliveries (
-    delivery_id SERIAL PRIMARY KEY,
-    delivery_date DATE NOT NULL,
-    delivered_quantity INTEGER NOT NULL,
-    receiver_id INTEGER REFERENCES employees(employee_id),
-    supplier_id INTEGER REFERENCES suppliers(supplier_id)
-);
-
--- Table FACTORIES
 CREATE TABLE factories (
     factory_id SERIAL PRIMARY KEY,
     factory_name VARCHAR(100) NOT NULL
 );
 
--- Table ROBOTS
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    age INTEGER,
+    start_date DATE NOT NULL,
+    end_date DATE,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    factory_id INTEGER REFERENCES factories(factory_id)
+);
+
+CREATE TABLE suppliers (
+    supplier_id SERIAL PRIMARY KEY,
+    supplier_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE deliveries (
+    delivery_id SERIAL PRIMARY KEY,
+    supplier_id INTEGER REFERENCES suppliers(supplier_id),
+    delivery_date DATE NOT NULL,
+    quantity INTEGER NOT NULL,
+    received_by INTEGER REFERENCES employees(employee_id),
+    factory_id INTEGER REFERENCES factories(factory_id)
+);
+
 CREATE TABLE robots (
     robot_id SERIAL PRIMARY KEY,
     robot_model VARCHAR(50) NOT NULL,
     produced_quantity INTEGER NOT NULL,
     parts_used INTEGER NOT NULL,
     factory_id INTEGER REFERENCES factories(factory_id)
+);
+
+CREATE TABLE audit_robot (
+    audit_id SERIAL PRIMARY KEY,
+    robot_id INTEGER REFERENCES robots(robot_id),
+    creation_date DATE NOT NULL DEFAULT CURRENT_DATE
 );
