1e7c05c3066ebe73059f96f7e8c2441f82230537
kropot <neooen07@gmail.com>
2024-06-19 15:58:50 +0200

    final

--- ws_rh/rh_app.py
+++ ws_rh/rh_app.py
@@ -31,74 +31,7 @@ def index():
     except Exception as e:
         app.logger.error(f"Error querying factories: {e}")
         return "An error occurred while fetching the data."
-
-    html = '''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Human Resources</title>
-        <style>
-            table {
-                text-align: center;
-                width: 100%;
-                border-collapse: collapse; 
-                border: 1px solid;
-            }
-
-            #container{
-                text-align: center;
-                width: 100%;
-            }
-
-            tr:hover {
-                background-color: #ddd;
-            }
-        </style>
-    </head>
-    <body>
-        <div id="container">
-            <h1>Welcome to the Human-Bot Project</h1>
-    '''
-
-    for factory in factories:
-        html += f'''
-            <h2>{factory.factory_name}</h2>
-            <table>
-                <tr>
-                    <th>FirstName</th>
-                    <th>LastName</th>
-                    <th>Age</th>
-                    <th>Start Date</th>
-                    <th>End Date / is active</th>
-                </tr>
-        '''
-
-        for employee in factory.employees:
-            html += f'''
-                <tr>
-                    <td>{employee.firstname}</td>
-                    <td>{employee.lastname}</td>
-                    <td>{employee.age}</td>
-                    <td>{employee.start_date}</td>
-                    <td>{employee.end_date if employee.end_date else '/'}</td>
-                    <td>{"Active" if employee.is_active else "Inactive"}</td>
-                </tr>
-            '''
-
-        html += '''
-            </table>
-        '''
-
-    html += '''
-        </div>
-    </body>
-    </html>
-    '''
-
-    return html
+    return render_template('rh.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
